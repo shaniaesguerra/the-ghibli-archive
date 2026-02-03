f4449c4d41d8f7e856214ab1c79b1eebf1d2be92
shaniaesguerra <shaniae47@gmail.com>
2026-02-03 10:51:24 -0700

    test get character data

--- scripts/main.js
+++ scripts/main.js
@@ -7,10 +7,10 @@ Studio Ghibli API
 Documentation: https://ghibliapi.dev/
 ******************************/
 //Gets Information about Ghibli Films. Returns an array of films
-const GHIBLI_FILMS_API_URL = "https://ghibliapi.dev/films"
+const GHIBLI_FILMS_API_URL = "https://ghibliapi.dev"
 async function fetchGhibliFilms() {
     try {
-        const response = await fetch('https://ghibliapi.dev/films');
+        const response = await fetch(`${GHIBLI_FILMS_API_URL}/films`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -23,6 +23,27 @@ async function fetchGhibliFilms() {
 }
 
 //fetchGhibliFilms();
+/******************************
+Studio Ghibli API
+Documentation: https://ghibliapi.dev/
+******************************/
+//Get Character Information. Returns all characters in ghibli
+async function fetchCharactersData() {
+    try {
+        const response = await fetch('https://ghibliapi.dev/people');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const characters = await response.json();
+        console.log(characters);
+
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+//fetchCharactersData();
+
 
 /******************************
 Jikan API
@@ -30,11 +51,11 @@ Documentation: https://docs.api.jikan.moe/
 ******************************/
 //Gets Studio Ghibli's Data:
 const STUDIO_GHIBLI_ID = 21;
-const JIKAN_API_URL = `https://api.jikan.moe/v4/producers/${STUDIO_GHIBLI_ID}/full`;
+const JIKAN_API_URL = `https://api.jikan.moe/v4`;
 
-async function fetchGhibliData() {
+async function fetchStudioGhibliData() {
     try {
-        const response = await fetch(JIKAN_API_URL);
+        const response = await fetch(`${JIKAN_API_URL}/producers/${STUDIO_GHIBLI_ID}/full`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -46,4 +67,4 @@ async function fetchGhibliData() {
     }
 }
 
-//fetchGhibliData();
+//fetchStudioGhibliData();
