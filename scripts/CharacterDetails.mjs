02b20dce24470c53606ad1f24236b03ec52cf2d5
shaniaesguerra <shaniae47@gmail.com>
2026-02-03 09:57:10 -0700

    chracter details set-up

---
+++ scripts/CharacterDetails.mjs
@@ -0,0 +1,34 @@
+import { renderCharacterTemplate } from "./utils.mjs";
+//Get information for Character Cards
+// -- establish utils export function and import it here
+/*
+ - Character Class
+ - members: container to append div, 
+ - init()
+ - displayAllCharacters()
+     - fetch all charactersfrom url
+     - display characters
+ - display a character( id)
+     - gets an id of the character
+     - can be used for single search
+ - renderCharacterTemplate()
+     - a template to display info for a character card
+ */
+
+function characterDetailsTemplate(character) {
+    
+}
+
+
+export default class Character {
+    constructor(containerElement, dataSource) {
+        this.dataSource = dataSource;
+        this.container = container;
+    }
+    init() {
+
+    }
+    displayAllCharacters(list) {
+
+    }
+}
--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -1,3 +1,17 @@
-export function renderCharacterTemplate() {
-    
+// retrieve data from localstorage
+export function getLocalStorage(key) {
+    return JSON.parse(localStorage.getItem(key));
+
+}
+// save data to local storage
+export function setLocalStorage(key, data) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+//Render data on template
+export function renderWithTemplate(template, parentElement, data, callback) {
+    parentElement.innerHTML = template;
+    if (callback) {
+        callback(data);
+    }
 }
