fa08f41259b80f75f7db04ce789b9d3d8a26782f
shaniaesguerra <shaniae47@gmail.com>
2026-02-03 12:10:32 -0700

    remove test code

--- scripts/main.js
+++ scripts/main.js
@@ -24,50 +24,6 @@ async function fetchGhibliFilms() {
 
 //fetchGhibliFilms();
 
-const STUDIO_GHIBLI_ID = 21;
-const JIKAN_API_URL = `https://api.jikan.moe/v4`;
-//Get Character Information. Returns all characters in ghibli
-async function fetchFilmsData() {
-    let currentPage = 1;
-    let allFilms = [];
-    let hasNextPage = true;
-
-    while (hasNextPage) {
-        try {
-            const response = await fetch(`${JIKAN_API_URL}/anime?producers=${STUDIO_GHIBLI_ID}&page=${currentPage}`);
-    
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const characters = await response.json();
-            //console.log(characters); //for debugging
-
-            //put data in list
-            characters.data.forEach(char => {
-                allFilms.push(char);
-            });
-
-            //check for another page
-            hasNextPage = characters.pagination.has_next_page;
-            
-            if (hasNextPage) {
-                //Wait between requests
-                currentPage++; //got to nextpage
-                await new Promise(resolve => setTimeout(resolve, 400));
-            }
-
-        } catch (error) {
-            console.error('Error fetching data:', error);
-            break;
-        }
-    }
-
-   console.log(allFilms); 
-}
-
-//fetchFilmsData();
-
 
 /******************************
 Jikan API
