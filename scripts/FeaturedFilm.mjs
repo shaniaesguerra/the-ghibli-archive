2937d2ac49dc57ed70dcbd8c06e9f77d7a0b60ab
shaniaesguerra <shaniae47@gmail.com>
2026-02-07 13:00:44 -0700

    fixing styling on main

--- scripts/FeaturedFilm.mjs
+++ scripts/FeaturedFilm.mjs
@@ -10,24 +10,21 @@ export function featuredFilmTemplate(film) {
             `;
         } else {
             return `
-            <div class="featureFilm-info">
+                < div class="featureFilm-info" >
                 <figure>
                     <img src="${film.images.webp.large_image_url}" alt="${film.title_english} Movie Poster">
                 </figure>
-                <h4>Synopsis:</h4>
-                <p>${film.synopsis}</p>
-            </div>
-
-            <div class="hero-container">
-                <div class="video">
-                <iframe width="560" height="315" src="${film.trailer.embed_url}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div class="hero-overlay-content">
+                <div class="title-heading">
                     <h1>Ghibli Film of the Day</h1>
                     <h2 class="film-title">${film.title}<br><span class="film-title-japanese">${film.title_japanese}</span></h2>
                 </div>
-            </div>
-            `;
+                <h4>Synopsis:</h4>
+                <p>${film.synopsis}</p>
+            </div >
+            <h3>👀 Watch the trailer ⤵️</h3>
+            <div class="video">
+                <iframe width="560" height="315" src="${film.trailer.embed_url}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>`;
         }
     }
     else {
@@ -44,18 +41,16 @@ export function featuredFilmTemplate(film) {
                 <figure>
                     <img src="${film.images.webp.large_image_url}" alt="${film.title_english} Movie Poster">
                 </figure>
-                <h4>Synopsis:</h4>
-                <p>${film.synopsis}</p>
-            </div>
-            
-            <div class="hero-container">
-                <div class="video">
-                <iframe width="560" height="315" src="${film.trailer.embed_url}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-                <div class="hero-overlay-content">
+                <div class="title-heading">
                     <h1>Ghibli Film of the Day</h1>
                     <h2 class="film-title">${film.title_english}<br><span class="film-title-japanese">${film.title_japanese}</span></h2>
                 </div>
+                <h4>Synopsis:</h4>
+                <p>${film.synopsis}</p>
+            </div>
+            <h3>👀 Watch the trailer ⤵️</h3>
+            <div class="video">
+                <iframe width="560" height="315" src="${film.trailer.embed_url}" frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             </div>`;
         }
     }
